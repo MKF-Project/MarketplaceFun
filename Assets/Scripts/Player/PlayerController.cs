9bdbf3ef2ef1b585dcfe4daa4aec935eb839c6a9
Pedro Ortiz Bledow <kynakerisk@gmail.com>
2021-06-07 18:46:29 -0300

    Implement multiplayer player camera

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAPI;
+
+public class PlayerController : NetworkBehaviour
+{
+    private Camera _playerCamera;
+
+    private void Awake()
+    {
+        _playerCamera = gameObject.GetComponentInChildren<Camera>();
+    }
+
+    private void Start()
+    {
+        if(IsOwner) {
+            _playerCamera.enabled = true;
+            ObjectsManager.OverviewCamera?.SetActive(false);
+        }
+    }
+
+    private void OnDestroy() {
+        if(IsOwner) {
+            ObjectsManager.OverviewCamera?.SetActive(true);
+        }
+    }
+}

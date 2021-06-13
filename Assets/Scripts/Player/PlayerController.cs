3648768bbdfa22ee27592eb3ee9a270de6e7bcb5
Pedro Ortiz Bledow <kynakerisk@gmail.com>
2021-06-13 19:57:59 -0300

    Start player without behaviour

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,24 +5,70 @@ using MLAPI;
 
 public class PlayerController : NetworkBehaviour
 {
+    // By default, players start with no movement or camera behaviour,
+    // because they are first instantiated on the menu scene
+
+    // This event allows a static class method call to inform all
+    // Player instances that they should update their behaviour state
+    private delegate void OnBehaviourChangeDelegate(bool behaviourEnabled);
+    private static event OnBehaviourChangeDelegate OnPlayerBehaviourChanged;
+
+    private static bool _playerBehaviourEnabled = false;
+    public static bool playerBehaviourEnabled
+    {
+        get => _playerBehaviourEnabled;
+        set
+        {
+            if(value != _playerBehaviourEnabled)
+            {
+                _playerBehaviourEnabled = value;
+                OnPlayerBehaviourChanged?.Invoke(value);
+            }
+        }
+    }
+
     private Camera _playerCamera;
+    private PlayerMovement _movementScript;
+    private CameraMove _cameraScript;
 
     private void Awake()
     {
         _playerCamera = gameObject.GetComponentInChildren<Camera>();
+
+        _movementScript = gameObject.GetComponent<PlayerMovement>();
+        _cameraScript = gameObject.GetComponent<CameraMove>();
+
+        // Listen on OnPlayerBehaviourChanged event
+        OnPlayerBehaviourChanged += updateBehaviourState;
     }
 
     private void Start()
     {
-        if(IsOwner) {
-            _playerCamera.enabled = true;
-            ObjectsManager.OverviewCamera?.SetActive(false);
-        }
+        // Set initial behaviour state for this player
+        updateBehaviourState(playerBehaviourEnabled);
     }
 
-    private void OnDestroy() {
-        if(IsOwner) {
-            ObjectsManager.OverviewCamera?.SetActive(true);
+    private void OnDestroy()
+    {
+        OnPlayerBehaviourChanged -= updateBehaviourState;
+
+        usePlayerCamera(false);
+    }
+
+    private void updateBehaviourState(bool behaviourEnabled)
+    {
+        _movementScript.enabled = behaviourEnabled;
+        _cameraScript.enabled = behaviourEnabled;
+
+        usePlayerCamera(behaviourEnabled);
+    }
+
+    private void usePlayerCamera(bool usePlayerCamera)
+    {
+        if(IsOwner)
+        {
+            _playerCamera.enabled = usePlayerCamera;
+            ObjectsManager.OverviewCamera?.SetActive(!usePlayerCamera);
         }
     }
 }

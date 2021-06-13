fea020b00fb63375abf0cc586eade5e14436a942
Pedro Ortiz Bledow <kynakerisk@gmail.com>
2021-06-12 23:52:36 -0300

    Create Extensions source file

---
+++ Assets/Scripts/Extensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public static class Extensions
+{
+    public static IEnumerable<GameObject> FindChildrenWithTag(this GameObject root, string tag, bool includeInactive = true)
+    {
+        return root.GetComponentsInChildren<Transform>(includeInactive).Where(child => child.tag == tag).Select(child => child.gameObject);
+    }
+}

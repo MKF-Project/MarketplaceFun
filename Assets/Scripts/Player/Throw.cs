6b0df53ebe751023e3772d73700cfdb20b60e859
Rodrigo Caetani <rocaetani@gmail.com>
2021-06-24 02:56:38 -0300

    Throw Fix - Clients now can throw

--- Assets/Scripts/Player/Throw.cs
+++ Assets/Scripts/Player/Throw.cs
@@ -1,7 +1,10 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using MLAPI;
+using MLAPI.Messaging;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Throw : NetworkBehaviour
 {
@@ -11,13 +14,25 @@ public class Throw : NetworkBehaviour
     public float Strength;
 
     public float ArcThrow;
+
+    private bool _isOn;
     
-    public Vector3 targetFake;
     //public Camera Camera;
 
     //public GameObject InstantiatedBomb;
     // Start is called before the first frame update
     private void Awake()
+    {
+        SceneManager.OnMatchLoaded += SpawnBomb;
+        _isOn = false;
+    }
+
+    private void OnDestroy()
+    {
+        SceneManager.OnMatchLoaded -= SpawnBomb;
+    }
+
+    private void SpawnBomb(string sceneName)
     {
         if (IsServer)
         {
@@ -26,30 +41,44 @@ public class Throw : NetworkBehaviour
             _bombRigidbody = _bomb.GetComponent<Rigidbody>();
             _bomb.SetActive(false);
         }
+
+        _isOn = true;
     }
 
     // Update is called once per frame
     private void Update()
     {
-        if (IsOwner)
+        if (IsOwner && _isOn)
         {
-            Vector3 screenMiddle = new Vector3();
-            screenMiddle.x = Screen.width / 2;
-            screenMiddle.y = Screen.height / 2;
-            Ray ray = Camera.main.ScreenPointToRay(screenMiddle);
-
-            Vector3 target = ray.direction * Strength;
-            target += Vector3.up * ArcThrow;
+            Vector3 initialPosition =  new Vector3(transform.position.x, transform.position.y + 3, transform.position.z);
+            Vector3 target = CalculateTargetPosition();
 
             if (InputManager.PressFireButton())
             {
-                _bombRigidbody.velocity = Vector3.zero;
-                _bomb.transform.position = new Vector3(transform.position.x, transform.position.y + 3, transform.position.z);
-                _bomb.SetActive(true);
-                _bombRigidbody.AddForce(target, ForceMode.Impulse);
-                targetFake = target;
+                ThrowServerRpc(target, initialPosition);
             }
         }
+    }
+
+    [ServerRpc]
+    public void ThrowServerRpc(Vector3 target, Vector3 initialPosition)
+    {
+        _bombRigidbody.velocity = Vector3.zero;
+        _bomb.transform.position = initialPosition;
+        _bomb.SetActive(true);
+        _bombRigidbody.AddForce(target, ForceMode.Impulse);
+    }
+
+    private Vector3 CalculateTargetPosition()
+    {
+        Vector3 screenMiddle = new Vector3();
+        screenMiddle.x = Screen.width / 2;
+        screenMiddle.y = Screen.height / 2;
+        Ray ray = Camera.main.ScreenPointToRay(screenMiddle);
+
+        Vector3 target = ray.direction * Strength;
+        target += Vector3.up * ArcThrow;
+        return target;
     }
 
 }

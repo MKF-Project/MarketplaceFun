0bce2c5c97a381e9a491054cbc8b3bee7d8973d9
Pedro Ortiz Bledow <kynakerisk@gmail.com>
2021-08-31 01:17:19 -0300

    Turn PlayerControls interface into abstract class In preparation of player interactions

--- Assets/Scripts/Player/Controls/PlayerControls.cs
+++ Assets/Scripts/Player/Controls/PlayerControls.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using MLAPI;
 
 public enum PlayerControlSchemes {
     None,
@@ -6,16 +7,94 @@ public enum PlayerControlSchemes {
     CartControls
 }
 
-public interface PlayerControls
+public abstract class PlayerControls : NetworkBehaviour
 {
-    void Move(Vector2 direction);
+    protected CharacterController _controller;
+    protected Transform _camera;
+    protected GameObject _currentLookingObject = null;
 
-    void Look(Vector2 direction);
+    protected Vector2 _currentDirection = Vector2.zero;
+    protected Vector2 _nextRotation = Vector2.zero;
 
-    void Jump();
+    protected bool _isWalking = false;
+    protected float _currentSpeed = 0;
 
-    void Walk();
+    public float MoveSpeed;
+    public float WalkSpeed;
+    public float Sensitivity;
+    public float InteractionDistance;
 
-    void Interact();
+    protected virtual void Awake()
+    {
+        _controller = gameObject.GetComponent<CharacterController>();
+
+        // Get the GameObject that contains the camera
+        _camera = gameObject.GetComponentInChildren<Camera>()?.transform;
+        #if UNITY_EDITOR
+            if(_camera == null)
+            {
+                Debug.LogError($"[{gameObject}::PlayerControls]: Player Camera not Found!");
+            }
+        #endif
+
+        _currentSpeed = MoveSpeed;
+
+        // Control Events
+        InputController.OnLook += Look;
+        InputController.OnMove += Move;
+        InputController.OnJump += Jump;
+        InputController.OnWalk += Walk;
+        InputController.OnInteractOrThrow += Interact;
+    }
+
+    protected virtual void OnDestroy()
+    {
+        // Control Events
+        InputController.OnLook -= Look;
+        InputController.OnMove -= Move;
+        InputController.OnJump -= Jump;
+        InputController.OnWalk -= Walk;
+        InputController.OnInteractOrThrow -= Interact;
+    }
+
+    protected virtual void FixedUpdate()
+    {
+
+    }
+
+    public virtual void Move(Vector2 direction)
+    {
+        if(IsOwner)
+        {
+            _currentDirection = direction;
+        }
+    }
+
+    public virtual void Look(Vector2 direction)
+    {
+        if(IsOwner)
+        {
+            _nextRotation = direction * Sensitivity;
+        }
+    }
+
+    public virtual void Jump()
+    {
+
+    }
+
+    public virtual void Walk()
+    {
+        if(IsOwner)
+        {
+            _isWalking = !_isWalking;
+            _currentSpeed = _isWalking? WalkSpeed : MoveSpeed;
+        }
+    }
+
+    public virtual void Interact()
+    {
+
+    }
 
 }

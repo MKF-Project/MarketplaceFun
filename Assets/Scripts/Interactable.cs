c7b5892ed62e1aefda9a3c897e2b5c36a7947563
Pedro Ortiz Bledow <kynakerisk@gmail.com>
2021-08-31 03:59:38 -0300

    Fix layerMask conversion in interactable

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -7,17 +7,27 @@ public class Interactable : NetworkBehaviour
 {
     public const string TAG_NAME = "Interactable";
     public const string LAYER_NAME = "Interact";
-    public static readonly int LAYER_MASK = LayerMask.NameToLayer(LAYER_NAME);
+
+    private static int _layerMask = -1;
+    public static int LAYER_MASK
+    {
+        get => _layerMask;
+    }
 
     private Collider _interactionCollider = null;
     private bool _configured = false;
 
     private void Awake()
     {
-        if(gameObject.tag != TAG_NAME || gameObject.layer != LAYER_MASK)
+        if(_layerMask == -1)
+        {
+            _layerMask = 1 << LayerMask.NameToLayer(LAYER_NAME);
+        }
+
+        if(gameObject.tag != TAG_NAME || 1 << gameObject.layer != LAYER_MASK)
         {
             #if UNITY_EDITOR
-                Debug.LogWarning($"[{gameObject}::Interactable]: Could not find suitable interactable gameobject. Tag: {gameObject.tag}, Layer: {LayerMask.LayerToName(gameObject.layer)}");
+                Debug.LogError($"[{gameObject}::Interactable]: Could not find suitable interactable gameobject. Tag: {gameObject.tag}, Layer: {LayerMask.LayerToName(gameObject.layer)}");
             #endif
 
             return;
@@ -31,10 +41,6 @@ public class Interactable : NetworkBehaviour
     public event OnLookEnterDelegate OnLookEnter;
     public void TriggerLookEnter(GameObject player)
     {
-        #if UNITY_EDITOR
-            Debug.Log($"[{gameObject}::OnLookEnter]: {player}, Configured: ${_configured}");
-        #endif
-
         if(_configured)
         {
             OnLookEnter?.Invoke(player);
@@ -45,10 +51,6 @@ public class Interactable : NetworkBehaviour
     public event OnLookExitDelegate OnLookExit;
     public void TriggerLookExit(GameObject player)
     {
-        #if UNITY_EDITOR
-            Debug.Log($"[{gameObject}::OnLookExit]: {player}, Configured: ${_configured}");
-        #endif
-
         if(_configured)
         {
             OnLookExit?.Invoke(player);
@@ -59,10 +61,6 @@ public class Interactable : NetworkBehaviour
     public event OnInteractDelegate OnInteract;
     public void TriggerInteract(GameObject player)
     {
-        #if UNITY_EDITOR
-            Debug.Log($"[{gameObject}::OnInteract]: {player}, Configured: ${_configured}");
-        #endif
-
         if(_configured)
         {
             OnInteract?.Invoke(player);

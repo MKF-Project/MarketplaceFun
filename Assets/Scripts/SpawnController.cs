64464dd7f8c8f04b513287d2211b593f1dd6025b
Pedro Ortiz Bledow <kynakerisk@gmail.com>
2021-10-18 19:27:28 -0300

    Create base code for Spawn countdown

--- Assets/Scripts/SpawnController.cs
+++ Assets/Scripts/SpawnController.cs
@@ -2,13 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 using MLAPI;
 using MLAPI.Messaging;
 
 public class SpawnController : NetworkBehaviour
 {
-    public const string SPAWN_BOUNDING_BOX_NAME = "BoundingBox";
-
     // Spawn logic
     public static int PlayerSpawnsRequired = 1;
     private HashSet<ulong> _playerInSpawn = new HashSet<ulong>();
@@ -19,12 +18,27 @@ public class SpawnController : NetworkBehaviour
     private Collider _playerPrefabCollider = null;
 
     // Collider vars
+    public const string SPAWN_BOUNDING_BOX_NAME = "BoundingBox";
+
     public PhysicMaterial BoundingBoxMaterial;
 
     private GameObject _boundingBox;
     private MeshFilter _boundingBoxMesh = null;
     private MeshRenderer _boundingBoxRenderer = null;
     private MeshCollider _generatedCollider;
+
+    // Release
+    private const string RELEASE_COUNTDOWN_NAME = "ReleaseCountdown";
+    private const string RELEASE_COUNTDOWN_MESSAGE = "Go!";
+
+    private const float COUNTDOWN_MESSAGE_HOLD_SECONDS = 1.5f;
+
+    private const int COUNTDOWN_SECONDS = 3;
+
+    private Text _releaseCountdown;
+
+    private static readonly WaitForSeconds OneSecondWait = new WaitForSeconds(1);
+    private static readonly WaitForSeconds HoldMessageWait = new WaitForSeconds(COUNTDOWN_MESSAGE_HOLD_SECONDS);
 
     private void Awake()
     {
@@ -57,6 +71,9 @@ public class SpawnController : NetworkBehaviour
 
         // Disable editor bounding box rendering
         _boundingBoxRenderer.enabled = false;
+
+        /* Release */
+        transform.Find(RELEASE_COUNTDOWN_NAME).TryGetComponent(out _releaseCountdown);
     }
 
     private void OnDestroy()
@@ -92,6 +109,24 @@ public class SpawnController : NetworkBehaviour
         );
     }
 
+    private IEnumerator PerformCountdown(int countdownSeconds)
+    {
+        _releaseCountdown.gameObject.SetActive(true);
+
+        for(int i = 1; i <= COUNTDOWN_SECONDS; i++)
+        {
+            _releaseCountdown.text = i.ToString();
+            yield return OneSecondWait;
+        }
+
+        _releaseCountdown.text = RELEASE_COUNTDOWN_MESSAGE;
+        yield return HoldMessageWait;
+
+        _releaseCountdown.gameObject.SetActive(false);
+        // TODO disable collider
+        // TODO Fire spawn release event
+    }
+
     /** ----- RPCs ----- **/
     [ServerRpc(RequireOwnership = false)]
     private void PlayerInSpawn_ServerRpc(ServerRpcParams rpcReceiveParams = default)
@@ -105,6 +140,7 @@ public class SpawnController : NetworkBehaviour
             if(_playerInSpawn.Count >= PlayerSpawnsRequired)
             {
                 print("RELEASE THE PLAYERS!");
+                ReleasePlayers_ClientRpc();
             }
         }
         #if UNITY_EDITOR
@@ -113,5 +149,11 @@ public class SpawnController : NetworkBehaviour
                 Debug.LogWarning($"[{gameObject.name}]: Tried to move player {rpcReceiveParams.Receive.SenderClientId} to SpawnArea multiple times!");
             }
         #endif
+    }
+
+    [ClientRpc]
+    private void ReleasePlayers_ClientRpc()
+    {
+        StartCoroutine(PerformCountdown(COUNTDOWN_SECONDS));
     }
 }

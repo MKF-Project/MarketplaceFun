8dddfe636d57694d5fb32fe58c0cb21bdbc918a8
Pedro Ortiz Bledow <kynakerisk@gmail.com>
2021-10-18 05:04:12 -0300

    Beginning of player teleport to spawn

--- Assets/Scripts/SpawnController.cs
+++ Assets/Scripts/SpawnController.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using MLAPI;
+using MLAPI.Messaging;
 
 public class SpawnController : NetworkBehaviour
 {
     public const string SPAWN_BOUNDING_BOX_NAME = "BoundingBox";
 
     // Spawn logic
-    public static int PlayerSpawns = 1;
+    public static int PlayerSpawnsRequired = 1;
+    private HashSet<ulong> _playerInSpawn = new HashSet<ulong>();
 
-    private int _playersCollected = 0;
+    private static SpawnController _levelSpawnInstance = null;
 
     // Collider vars
     public PhysicMaterial BoundingBoxMaterial;
@@ -22,6 +24,19 @@ public class SpawnController : NetworkBehaviour
 
     private void Awake()
     {
+        /* Spawn Logic */
+        if(_levelSpawnInstance != null)
+        {
+            #if UNITY_EDITOR
+                Debug.LogError($"[{gameObject.name}]: More than one SpawnArea in the level! This SpawnArea will be disabled");
+            #endif
+
+            Destroy(gameObject);
+            return;
+        }
+        _levelSpawnInstance = this;
+
+        /* Generate Collider */
         _boundingBox = transform.Find(SPAWN_BOUNDING_BOX_NAME).gameObject;
 
         _boundingBox.TryGetComponent(out _boundingBoxMesh);
@@ -40,8 +55,48 @@ public class SpawnController : NetworkBehaviour
         _boundingBoxRenderer.enabled = false;
     }
 
+    private void OnDestroy()
+    {
+        if(_levelSpawnInstance == this)
+        {
+            _levelSpawnInstance = null;
+        }
+    }
+
     private void Start()
     {
+        // Since this procedure will be run when the level loads,
+        // we can be assured that a new player has finished loading their local
+        // version of the level
+        if(IsClient)
+        {
+            // TODO add randomness to spawn location, keeping it inside bbox
+            NetworkController.SelfPlayer?.Teleport(transform.position);
 
+            PlayerInSpawn_ServerRpc();
+        }
+    }
+
+    /** ----- RPCs ----- **/
+    [ServerRpc(RequireOwnership = false)]
+    private void PlayerInSpawn_ServerRpc(ServerRpcParams rpcReceiveParams = default)
+    {
+        if(!_playerInSpawn.Contains(rpcReceiveParams.Receive.SenderClientId))
+        {
+            print($"Added player {rpcReceiveParams.Receive.SenderClientId} to spawn");
+            _playerInSpawn.Add(rpcReceiveParams.Receive.SenderClientId);
+
+            // Check for game start
+            if(_playerInSpawn.Count >= PlayerSpawnsRequired)
+            {
+                print("RELEASE THE PLAYERS!");
+            }
+        }
+        #if UNITY_EDITOR
+            else
+            {
+                Debug.LogWarning($"[{gameObject.name}]: Tried to move player {rpcReceiveParams.Receive.SenderClientId} to SpawnArea multiple times!");
+            }
+        #endif
     }
 }

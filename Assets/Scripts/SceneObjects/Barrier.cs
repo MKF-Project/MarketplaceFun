8fa5044a1489fe53591f8de80cc8bcddd3ff233f
Pedro Ortiz Bledow <kynakerisk@gmail.com>
2021-10-19 02:28:23 -0300

    Create Barrier Prefab to demonstrate Spawn event

---
+++ Assets/Scripts/SceneObjects/Barrier.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Barrier : MonoBehaviour
+{
+    public float DescendSpeed;
+    public float MoveDistance;
+
+    private bool _openGate = false;
+    private float _targetHeight;
+
+    private void Awake()
+    {
+        _targetHeight = transform.position.y - MoveDistance;
+
+        SpawnController.OnSpawnOpened += OpenBarrier;
+    }
+
+    private void Update()
+    {
+        if(_openGate && transform.position.y > _targetHeight)
+        {
+            transform.position += Vector3.down * DescendSpeed * Time.deltaTime;
+        }
+    }
+
+    private void OpenBarrier()
+    {
+        SpawnController.OnSpawnOpened -= OpenBarrier;
+
+        _openGate = true;
+    }
+}

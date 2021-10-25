26e7f5a85925f90bd6bfdfe6196cd81baeceb2e8
Pedro Ortiz Bledow <kynakerisk@gmail.com>
2021-10-25 04:12:29 -0300

    Remove MLAPI.Messaging from NetworkItemManager, as it is not a NetworkBehaviour, and so cannot contains RPCs

--- Assets/Scripts/Network/NetworkItemManager.cs
+++ Assets/Scripts/Network/NetworkItemManager.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using MLAPI;
-using MLAPI.Messaging;
 using MLAPI.Configuration;
 
 public class NetworkItemManager : MonoBehaviour

97098674d8d38f08b9a07aa697404046923cc8fe
Pedro Ortiz Bledow <kynakerisk@gmail.com>
2021-12-08 20:28:12 -0300

    Create PlayerAudio script

---
+++ Assets/Scripts/Player/PlayerAudio.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAudio : MonoBehaviour
+{
+    [Header("Sounds")]
+    public List<AudioClip> Steps;
+
+    private AudioSource _playerSource;
+    private Player _playerScript;
+
+    private void Awake()
+    {
+        TryGetComponent(out _playerScript);
+        _playerSource = GetComponentInChildren<AudioSource>();
+    }
+
+    private void Start()
+    {
+        if(_playerScript.IsOwner)
+        {
+            _playerSource.spatialBlend = 0;
+        }
+    }
+
+    // Call Sound Functions
+    public void AnimationStepForward(AnimationEvent caller)
+    {
+        if(caller.animatorClipInfo.weight > 0.5f)
+        {
+            PlayStep();
+        }
+    }
+
+    public void AnimationStepSideways(AnimationEvent caller)
+    {
+        if(caller.animatorClipInfo.weight >= 0.5f)
+        {
+            PlayStep();
+        }
+    }
+
+    public void PlayStep() => _playerSource.PlayOneShot(Steps[Random.Range(0, Steps.Count)]);
+}

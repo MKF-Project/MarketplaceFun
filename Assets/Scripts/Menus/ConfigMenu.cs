09e57827283798a44cb10989dee72d9e3f907b9c
Pedro Ortiz Bledow <kynakerisk@gmail.com>
2021-12-12 02:26:16 -0300

    Create Configurations Menu

---
+++ Assets/Scripts/Menus/ConfigMenu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ConfigMenu : MonoBehaviour
+{
+
+    public static float Sensitivity = 1;
+
+    // Events
+    public delegate void OnPressOKDelegate();
+    public static event OnPressOKDelegate OnPressOK;
+
+    [SerializeField] private Slider SensitivitySlider = null;
+    [SerializeField] private Text SensitivityValue = null;
+
+    private void Awake()
+    {
+        onChangeSliderValue();
+
+        GameMenu.OnPressConfigurations += this.toggleMenu;
+    }
+
+    private void OnDestroy()
+    {
+        GameMenu.OnPressConfigurations -= this.toggleMenu;
+    }
+
+    public void onChangeSliderValue()
+    {
+        Sensitivity = SensitivitySlider.value;
+        SensitivityValue.text = Sensitivity.ToString("0.00");
+    }
+
+    // Button Actions
+    public void pressOK() => OnPressOK?.Invoke();
+}
